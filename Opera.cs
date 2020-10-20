bafdd05dd6db0206c823d4e3c001941b19174388
Dany <Dany@DANY-PC>
2020-10-19 17:53:50 -0700

    Agregar archivos de proyecto.

---
+++ Opera.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibreríaPractica
+{
+    public class Opera
+    {
+        public static double Suma(double n1, double n2)
+        {
+            return n1 + n2;
+        }
+
+        public static double Resta(double n1, double n2)
+        {
+            return n1 - n2;
+        }
+
+        public static double Multi(double n1, double n2)
+        {
+            return n1 * n2;
+        }
+
+        public static double Divide(double n1, double n2)
+        {
+            return n1 / n2;
+        }
+    }
+}
